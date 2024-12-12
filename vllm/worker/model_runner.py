456819444d8c0873658fbc6bc8b9236307733809
Ray Wan <raywan@berkeley.edu>
2024-12-12 17:21:36 +0900

    finished

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -997,7 +997,8 @@ class ModelInputForGPUBuilder(ModelRunnerInputBuilderBase[ModelInputForGPU]):
         if self.enable_control_vector:
             control_vector_requests = set(
                 data.control_vector_request for data in self.inter_data_list
-                if data.control_vector_request)  ##TODO:figure this out
+                if data.control_vector_request
+            )
 
         # Multi-modal data.
         multi_modal_kwargs_list = [
