9a5680761e2b153759674b768adda7a645068bc0
Yuu Momma <yuu.monma.bh@nttdocomo.com>
2025-04-04 14:53:51 +0900

    Fix mypy type check error
    Signed-off-by: Yuu Momma <yuu.monma.bh@nttdocomo.com>

--- vllm/engine/multiprocessing/client.py
+++ vllm/engine/multiprocessing/client.py
@@ -17,6 +17,7 @@ from zmq.asyncio import Socket
 
 from vllm import PoolingParams
 from vllm.config import DecodingConfig, ModelConfig, VllmConfig
+from vllm.control_vectors.request import ControlVectorRequest
 from vllm.core.scheduler import SchedulerOutputs
 # yapf conflicts with isort for this block
 # yapf: disable
@@ -449,6 +450,7 @@ class MQLLMEngineClient(EngineClient):
         lora_request: Optional[LoRARequest] = None,
         trace_headers: Optional[Mapping[str, str]] = None,
         prompt_adapter_request: Optional[PromptAdapterRequest] = None,
+        control_vector_request: Optional[ControlVectorRequest] = None,
         priority: int = 0,
     ) -> AsyncGenerator[RequestOutput, None]:
         """Generate outputs for a request.
@@ -679,3 +681,8 @@ class MQLLMEngineClient(EngineClient):
         # Raise on error, otherwise happily return None
         if isinstance(request_output, BaseException):
             raise request_output
+
+    async def add_control_vector(self,
+                                 cv_request: ControlVectorRequest) -> None:
+        """Load a new control vector into the engine for future requests."""
+        pass
